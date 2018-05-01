f086ada75a73fb5e116992782754d3304170c513
Robert M Flight <r.flight79@gmail.com>
2018-05-01 09:59:56 -0400

    testing the filtering

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(smirfeTools)
+
+test_check("smirfeTools")
---
+++ tests/testthat/test-assignments.R
@@ -0,0 +1,7 @@
+context("test-assignments.R")
+
+test_that("filtering assignments works", {
+  data(assigned_data)
+  tmp <- filter_assignments(assigned_data[[1]]$assignments)
+  expect_equal(sum(tmp$Type %in% "Secondary"), 0)
+})
