d10c5bc37f91403352add20b2fecaa3a56addd90
rmflight <r.flight79@gmail.com>
2019-04-02 14:14:59 -0400

    doing some basic IMF matching for those samples that don't have the voted EMF

--- playing_with_emf_voting.R
+++ playing_with_emf_voting.R
@@ -1,6 +1,8 @@
 # helping me figure out labeled EMF voting
 library(dplyr)
 library(ggplot2)
+library(furrr)
+plan(multiprocess)
 #assigned_data = readRDS("unlabeled_example_zaytseva_pdx.rds")
 assigned_data = readRDS("lung_matched_tissue_raw_smirfe_assignments_2018-09-07_imf_adduct_fusion1_cancer.rds")
 
@@ -8,12 +10,14 @@ assigned_data = readRDS("lung_matched_tissue_raw_smirfe_assignments_2018-09-07_i
 # multiple mapping drops.
 # I should also be asking how many of the grouped EMFs have different numbers of peaks across samples...
 
+peak_mz = purrr::map_df(assigned_data, ~ dplyr::filter(.x$data, Measurement %in% "ObservedMZ"))
+
 get_sample_emfs = function(sample_assignments, sample_id, evalue_cutoff = 0.98){
-  sample_assignments = dplyr::mutate(sample_assignments, emf.adduct = paste0(complete_EMF, ".", adduct_IMF))
+  sample_assignments = dplyr::mutate(sample_assignments, emf_Adduct = paste0(complete_EMF, ".", adduct_IMF))
   e_values = dplyr::filter(sample_assignments, Type %in% "e_value") %>% dplyr::mutate(e_value = as.numeric(Assignment_Data))
   clique_size = dplyr::filter(sample_assignments, Type %in% "clique_size") %>% dplyr::mutate(clique_size = as.integer(Assignment_Data))
 
-  peaks_by_emf = split(sample_assignments$PeakID, sample_assignments$emf.adduct) %>%
+  peaks_by_emf = split(sample_assignments$PeakID, sample_assignments$emf_Adduct) %>%
     purrr::map2_dfr(., names(.), function(.x, .y){
       peaks = unique(.x)
       tmp_frame = data.frame(emf = .y, peak_chr = paste(peaks, collapse = ","), stringsAsFactors = FALSE)
@@ -27,9 +31,10 @@ get_sample_emfs = function(sample_assignments, sample_id, evalue_cutoff = 0.98){
     #message(ige)
     x = grouped_emf[[ige]]
     use_peaks = x$peaks[1][[1]]
-    peak_info = purrr::map(x$emf, ~ dplyr::filter(sample_assignments, PeakID %in% use_peaks, emf.adduct %in% .x))
-    grouped_evalues = purrr::map_dbl(x$emf, ~ dplyr::filter(e_values, PeakID %in% use_peaks, emf.adduct %in% .x) %>% dplyr::slice(which.min(e_value)) %>% dplyr::pull(e_value))
-    clique_size = purrr::map_int(x$emf, ~ dplyr::filter(clique_size, PeakID %in% use_peaks, emf.adduct %in% .x) %>% dplyr::slice(1) %>% dplyr::pull(clique_size))
+    peak_info = purrr::map(x$emf, ~ dplyr::filter(sample_assignments, PeakID %in% use_peaks, emf_Adduct %in% .x))
+    names(peak_info) = x$emf
+    grouped_evalues = purrr::map_dbl(x$emf, ~ dplyr::filter(e_values, PeakID %in% use_peaks, emf_Adduct %in% .x) %>% dplyr::slice(which.min(e_value)) %>% dplyr::pull(e_value))
+    clique_size = purrr::map_int(x$emf, ~ dplyr::filter(clique_size, PeakID %in% use_peaks, emf_Adduct %in% .x) %>% dplyr::slice(1) %>% dplyr::pull(clique_size))
 
     keep_emf = grouped_evalues <= evalue_cutoff
     x = x[keep_emf, ]
@@ -40,7 +45,7 @@ get_sample_emfs = function(sample_assignments, sample_id, evalue_cutoff = 0.98){
            min_e_value = min(grouped_evalues[keep_emf]),
            clique_size = clique_size[1],
            peaks = use_peaks,
-           peaks_chr = paste0(peak_info[[1]]$Sample[1], ".", use_peaks)
+           Sample_Peak = paste0(peak_info[[1]]$Sample[1], "_", use_peaks)
       ))
     } else {
       return(NULL)
@@ -53,14 +58,14 @@ get_sample_emfs = function(sample_assignments, sample_id, evalue_cutoff = 0.98){
   names(grouped_emf_peaks) = paste0("GEMF_", seq_along(grouped_emf_peaks), ".", sample_id)
 
   emf_by_emf = purrr::map_dfr(grouped_emf_peaks, function(x){
-    purrr::map_dfr(x$peak_info, ~ data.frame(emf.adduct = .x$emf.adduct[1],
+    purrr::map_dfr(x$peak_info, ~ data.frame(emf_Adduct = .x$emf_Adduct[1],
                                              isotopologue_EMF = unique(dplyr::filter(.x, Type %in% "isotopologue_EMF") %>% dplyr::pull(Assignment_Data)), stringsAsFactors = FALSE))
   }) %>% split(., .$isotopologue_EMF)
 
   multi_evidence_emf = purrr::map(emf_by_emf, function(x){
     out_data = NULL
     if (nrow(x) > 1) {
-      adducts = strsplit(x$emf.adduct, ".", fixed = TRUE) %>% purrr::map_chr(~ .x[2])
+      adducts = strsplit(x$emf_Adduct, ".", fixed = TRUE) %>% purrr::map_chr(~ .x[2])
 
       if (sum(adducts %in% c("1H1", "14N1,1H4")) != length(adducts)) {
         out_data = x
@@ -77,8 +82,6 @@ get_sample_emfs = function(sample_assignments, sample_id, evalue_cutoff = 0.98){
   # we should add the peaks to GEMF mapping here, so we can do what is noted on the last line.
 }
 
-library(furrr)
-plan(multiprocess)
 within_sample_emfs = furrr::future_map(assigned_data, function(.x){
   tmp_assign = dplyr::filter(.x$assignments, !grepl("S", complete_EMF))
   get_sample_emfs(tmp_assign, .x$sample)
@@ -87,7 +90,7 @@ within_sample_emfs = furrr::future_map(assigned_data, function(.x){
 
 all_gemf_emf_mapping = furrr::future_map_dfr(within_sample_emfs, function(x){
   purrr::map2_dfr(x$grouped_emf, names(x$grouped_emf), function(.x, .y){
-    data.frame(grouped_emf = .y, emf.adduct = .x$emf, stringsAsFactors = FALSE)
+    data.frame(grouped_emf = .y, emf_Adduct = .x$emf, stringsAsFactors = FALSE)
   })
 })
 
@@ -96,20 +99,20 @@ sudo_emf_list = vector("list", length(unique(all_gemf_emf_mapping$grouped_emf)))
 i_sudo = 1
 while (nrow(all_gemf_emf_mapping) > 0) {
   #message(i_sudo)
-  tmp_emf_mapping = dplyr::filter(all_gemf_emf_mapping, emf.adduct %in% all_gemf_emf_mapping$emf.adduct[1])
+  tmp_emf_mapping = dplyr::filter(all_gemf_emf_mapping, emf_Adduct %in% all_gemf_emf_mapping$emf_Adduct[1])
 
   emf_iter = dplyr::left_join(tmp_emf_mapping, all_gemf_emf_mapping, by = "grouped_emf") %>%
-    dplyr::transmute(grouped_emf = grouped_emf, emf.adduct = emf.adduct.y) %>% unique()
+    dplyr::transmute(grouped_emf = grouped_emf, emf_Adduct = emf_Adduct.y) %>% unique()
 
-  adduct_iter = dplyr::left_join(emf_iter, all_gemf_emf_mapping, by = "emf.adduct") %>%
-    dplyr::transmute(grouped_emf = grouped_emf.y, emf.adduct = emf.adduct) %>% unique()
+  adduct_iter = dplyr::left_join(emf_iter, all_gemf_emf_mapping, by = "emf_Adduct") %>%
+    dplyr::transmute(grouped_emf = grouped_emf.y, emf_Adduct = emf_Adduct) %>% unique()
 
   while (nrow(adduct_iter) != nrow(emf_iter)) {
     emf_iter = dplyr::left_join(adduct_iter, all_gemf_emf_mapping, by = "grouped_emf") %>%
-      dplyr::transmute(grouped_emf = grouped_emf, emf.adduct = emf.adduct.y) %>% unique()
+      dplyr::transmute(grouped_emf = grouped_emf, emf_Adduct = emf_Adduct.y) %>% unique()
 
-    adduct_iter = dplyr::left_join(emf_iter, all_gemf_emf_mapping, by = "emf.adduct") %>%
-      dplyr::transmute(grouped_emf = grouped_emf.y, emf.adduct = emf.adduct) %>% unique()
+    adduct_iter = dplyr::left_join(emf_iter, all_gemf_emf_mapping, by = "emf_Adduct") %>%
+      dplyr::transmute(grouped_emf = grouped_emf.y, emf_Adduct = emf_Adduct) %>% unique()
   }
 
   sudo_emf_list[[i_sudo]] = adduct_iter
@@ -138,7 +141,7 @@ extract_e_value_mass_error = function(peak_info, gemf_id, best_evalue = FALSE){
 }
 
 emf_peak_mapping = purrr::map2_dfr(all_gemfs, names(all_gemfs), function(.x, .y){
-  data.frame(emf = .y, peaks = .x$peaks_chr, stringsAsFactors = FALSE)
+  data.frame(emf = .y, peaks = .x$Sample_Peak, stringsAsFactors = FALSE)
 })
 
 # look at the variation of e-value with respect to m/z
@@ -198,11 +201,48 @@ by_sample = data.frame(grouped_emf = unique(single_sudo$grouped_emf), stringsAsF
                 sample = stringr::str_split_fixed(grouped_emf, "\\.", 2)[,2]) %>%
   dplyr::arrange(sample)
 
+#' match peaks to IMFs by M/Z
+#'
+#' Given a data.frame of IMFs to peaks for a given EMF, attempts to match unknown peaks
+#' to IMFs based on differences in average M/Z to the unknown peaks.
+#'
+#' @param imf_2_peak data.frame with IMF information (see details)
+#' @param unknown_peaks character vector of peak ids
+#' @param peak_mz data.frame with `Sample_Peak` and `Value`, where `Value` is `ObservedMZ`
+#'
+#' @return data.frame
+#' @export
+match_imf_by_mz = function(imf_2_peak, unknown_peaks, peak_mz){
+  mean_mz = purrr::map_dbl(split(imf_2_peak$Sample_Peak, imf_2_peak$complete_IMF),
+                       ~ mean(peak_mz[peak_mz$Sample_Peak %in% .x, "Value"]))
+
+  match_mz = purrr::map_df(unknown_peaks, function(test_peak){
+    test_mz = dplyr::filter(peak_mz, Sample_Peak %in% test_peak)
+    diff_mz = abs(test_mz$Value - mean_mz)
+    match_imf = data.frame(complete_IMF = names(diff_mz)[which.min(diff_mz)],
+                           PeakID = test_mz$PeakID,
+                           Sample = test_mz$Sample,
+                           Sample_Peak = test_peak,
+                           emf_Adduct = imf_2_peak$emf_Adduct[1],
+                           diff_mz = diff_mz[which.min(diff_mz)],
+                           stringsAsFactors = FALSE,
+                           row.names = FALSE)
+  })
+
+  # check if each row is unique, which means we have 1-1 peak to IMF
+  if (length(unique(match_mz$complete_IMF)) == nrow(match_mz)) {
+    match_mz$diff_mz = NULL
+  } else {
+    match_mz = match_mz[1,]
+    match_mz[1, ] = NA
+  }
+  match_mz
+}
 
 # this function does not account for the fact that a peak may be mapped to several EMFs
 # This is merely to choose amongst a number of EMFs that may be possible for a single set
 # of peaks
-choose_emf = function(grouped_emfs, keep_ratio = 0.9){
+choose_emf = function(grouped_emfs, peak_mz, keep_ratio = 0.9){
   grouped_evalues = purrr::map_df(grouped_emfs, ~ .x$e_values) %>% dplyr::mutate(information = 1 - e_value)
 
   # I think the way to incorporate multi-adduct evidence is to add some small constant value to each
@@ -214,12 +254,59 @@ choose_emf = function(grouped_emfs, keep_ratio = 0.9){
     dplyr::mutate(max_ratio = sum_information / max(sum_information))
   keep_emf = dplyr::filter(emf_votes, max_ratio >= keep_ratio)
 
+  null_evalue = data.frame(emf = NA, e_value = NA, stringsAsFactors = FALSE)
   gemf_emf = purrr::map2_dfr(grouped_emfs, names(grouped_emfs), function(.x, .y){
-    tmp_df = data.frame(grouped_emf = .y, emf = .x$emf, stringsAsFactors = FALSE)
-    tmp_df$peaks = list(.x$peaks)
-    tmp_df
-  })
+    sample_from_grouped_emf = stringr::str_split_fixed(.y, "\\.", 2)[2]
+    e_values = dplyr::filter(.x$e_values, emf %in% keep_emf$emf) %>% dplyr::select(emf, e_value)
 
-  matching_keep_emf = dplyr::filter(gemf_emf, emf %in% keep_emf$emf)
+    if (nrow(e_values) > 0) {
+      e_values$info = vector(mode = "list", length = nrow(e_values))
+      e_values$peaks = vector(mode = "list", length = nrow(e_values))
+      use_info = .x$peak_info[e_values$emf]
+      e_values$info = purrr::map(use_info, function(in_info){
+        in_info %>% dplyr::select(complete_IMF, PeakID, Sample, Sample_Peak, emf_Adduct) %>%
+          unique()
+      })
+
+      e_values$peaks = list(.x$Sample_Peak)
+      e_values$sample = sample_from_grouped_emf
+      e_values$grouped_emf = .y
+    } else {
+      e_values = null_evalue
+      e_values$info = vector(mode = "list", length = nrow(e_values))
+      e_values$peaks = list(.x$Sample_Peak)
+      e_values$sample = sample_from_grouped_emf
+      e_values$grouped_emf = .y
+    }
+    e_values
+  })
+  has_null_peaks = purrr::map_lgl(gemf_emf$e_value, is.na)
+  original_samples = sort(unique(gemf_emf$sample))
+  non_null_samples = sort(unique(gemf_emf$sample[!has_null_peaks]))
+
+  missing_samples = setdiff(original_samples, non_null_samples)
+  if (length(missing_samples) > 0) {
+    has_imf = purrr::map_df(gemf_emf[gemf_emf$sample %in% non_null_samples, "info"], ~ .x)
+    missing_imf = purrr::map(gemf_emf[(gemf_emf$sample %in% missing_samples), "peaks"], ~ .x)
+    names(missing_imf) = missing_samples
+
+    imf_by_emf = split(has_imf, has_imf$emf_Adduct)
+
+    imf_matches = purrr::map_df(purrr::cross2(imf_by_emf, missing_imf), function(x){
+      match_imf_by_mz(x[[1]], x[[2]], peak_mz)
+    })
+  }
+  gemf_emf
 
 }
+
+names(sudo_emf_list) = paste0("SEMF.", seq(1, length(sudo_emf_list)))
+# chosen_emfs = purrr::map2(sudo_emf_list[1:100], names(sudo_emf_list)[1:100], function(.x, .y){
+#   message(.y)
+#   choose_emf(all_gemfs[unique(.x$grouped_emf)])
+# })
+#
+# # debugging
+grouped_emfs = all_gemfs[unique(sudo_emf_list[[16]]$grouped_emf)]
+
+# chosen_emfs = furrr::future_map(sudo_emf_list, ~ choose_emf(all_gemfs[unique(.x$grouped_emf)]))
