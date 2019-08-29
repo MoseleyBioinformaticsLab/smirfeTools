8b8a187e22bce37ca17be2db411a9a1d01c0d1ef
rmflight <r.flight79@gmail.com>
2019-08-29 15:09:05 -0400

    updated memory logging function

--- R/logging.R
+++ R/logging.R
@@ -7,21 +7,28 @@
 #' @return NULL
 log_memory = function(){
   linux_memory = system("cat /proc/meminfo", intern = TRUE)
-  linux_memory = grep("^Mem", linux_memory, value = TRUE)
+  linux_memory = grep("^MemTotal|^MemAvailable|^Active|^SwapTotal|^SwapFree", linux_memory, value = TRUE)
+  linux_memory = grep("anon|active|file", linux_memory, value = TRUE, invert = TRUE)
 
   memory_values = stringr::str_extract(linux_memory, "[[:digit:]].*")
   memory_numbers = as.numeric(stringr::str_extract(memory_values, "[[:digit:]].* "))
-  memory_string = paste0("Memory: ", paste(paste(c("Total: ", "Free: ", "Available: "), memory_values, sep = ""), collapse = ", "))
+  memory_ids = stringr::str_extract(linux_memory, "^[[:alpha:]]+")
+  names(memory_numbers) = memory_ids
+  memory_string = paste0("Memory: ", paste(paste(c("Total: ", "Available: ", "Active: ", "SwapTotal: ", "SwapFree: "), memory_values, sep = ""), collapse = ", "))
 
-  if (any(memory_numbers == 0)) {
+  active_to_total = memory_numbers["Active"] / memory_numbers["MemTotal"]
+  swapfree_to_swap = memory_numbers["SwapFree"] / memory_numbers["SwapTotal"]
+
+  if ((active_to_total >= 0.95) || (swapfree_to_swap <= 0.95)) {
     if (get("logger", envir = has_logger)) {
-      logger::log_warn(memory_string, namespace = "smirfeTools")
+      memory_string2 = paste0("HIGH MEMORY USAGE!!! ", memory_string)
+      logger::log_warn(memory_string2, namespace = "FTMS.peakCharacterization")
     } else {
       warning(memory_string)
     }
   } else {
     if (get("logger", envir = has_logger)) {
-      logger::log_info(memory_string, namespace = "smirfeTools")
+      logger::log_info(memory_string, namespace = "FTMS.peakCharacterization")
     }
   }
 }
