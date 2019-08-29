8518e1bb7a17402e21dd14cfa3cdb99b626de9d5
rmflight <r.flight79@gmail.com>
2019-08-29 15:26:56 -0400

    only logging memory when on Linux

--- R/logging.R
+++ R/logging.R
@@ -6,31 +6,34 @@
 #' @export
 #' @return NULL
 log_memory = function(){
-  linux_memory = system("cat /proc/meminfo", intern = TRUE)
-  linux_memory = grep("^MemTotal|^MemAvailable|^Active|^SwapTotal|^SwapFree", linux_memory, value = TRUE)
-  linux_memory = grep("anon|active|file", linux_memory, value = TRUE, invert = TRUE)
+  if (get("memory", envir = has_logger)) {
+    linux_memory = system("cat /proc/meminfo", intern = TRUE)
+    linux_memory = grep("^MemTotal|^MemAvailable|^Active|^SwapTotal|^SwapFree", linux_memory, value = TRUE)
+    linux_memory = grep("anon|active|file", linux_memory, value = TRUE, invert = TRUE)
 
-  memory_values = stringr::str_extract(linux_memory, "[[:digit:]].*")
-  memory_numbers = as.numeric(stringr::str_extract(memory_values, "[[:digit:]].* "))
-  memory_ids = stringr::str_extract(linux_memory, "^[[:alpha:]]+")
-  names(memory_numbers) = memory_ids
-  memory_string = paste0("Memory: ", paste(paste(c("Total: ", "Available: ", "Active: ", "SwapTotal: ", "SwapFree: "), memory_values, sep = ""), collapse = ", "))
+    memory_values = stringr::str_extract(linux_memory, "[[:digit:]].*")
+    memory_numbers = as.numeric(stringr::str_extract(memory_values, "[[:digit:]].* "))
+    memory_ids = stringr::str_extract(linux_memory, "^[[:alpha:]]+")
+    names(memory_numbers) = memory_ids
+    memory_string = paste0("Memory: ", paste(paste(c("Total: ", "Available: ", "Active: ", "SwapTotal: ", "SwapFree: "), memory_values, sep = ""), collapse = ", "))
 
-  active_to_total = memory_numbers["Active"] / memory_numbers["MemTotal"]
-  swapfree_to_swap = memory_numbers["SwapFree"] / memory_numbers["SwapTotal"]
+    active_to_total = memory_numbers["Active"] / memory_numbers["MemTotal"]
+    swapfree_to_swap = memory_numbers["SwapFree"] / memory_numbers["SwapTotal"]
 
-  if ((active_to_total >= 0.95) || (swapfree_to_swap <= 0.95)) {
-    if (get("logger", envir = has_logger)) {
-      memory_string2 = paste0("HIGH MEMORY USAGE!!! ", memory_string)
-      logger::log_warn(memory_string2, namespace = "FTMS.peakCharacterization")
+    if ((active_to_total >= 0.95) || (swapfree_to_swap <= 0.95)) {
+      if (get("logger", envir = has_logger)) {
+        memory_string2 = paste0("HIGH MEMORY USAGE!!! ", memory_string)
+        logger::log_warn(memory_string2, namespace = "FTMS.peakCharacterization")
+      } else {
+        warning(memory_string)
+      }
     } else {
-      warning(memory_string)
-    }
-  } else {
-    if (get("logger", envir = has_logger)) {
-      logger::log_info(memory_string, namespace = "FTMS.peakCharacterization")
+      if (get("logger", envir = has_logger)) {
+        logger::log_info(memory_string, namespace = "FTMS.peakCharacterization")
+      }
     }
   }
+
 }
 
 #' log messages
--- R/zzz.R
+++ R/zzz.R
@@ -16,15 +16,20 @@ set_internal_map <- function(map_function = NULL){
 
 
 internal_map <- new.env(hash = TRUE)
+assign("map_function", purrr::map, envir = internal_map)
 has_logger = new.env(hash = TRUE)
 assign("logger", FALSE, envir = has_logger)
-assign("map_function", purrr::map, envir = internal_map)
+assign("memory", FALSE, envir = has_logger)
 
 .onLoad <- function(libname, pkgname) {
   tmp_packages = installed.packages()
   if ("logger" %in% rownames(tmp_packages)) {
     assign("logger", TRUE, envir = has_logger)
-    logger::log_appender(logger::appender_file(paste0("smirfetools_run_", substring(make.names(Sys.time()), 2), ".log")), namespace = "smirfeTools")
+    sys_info = Sys.info()
+    if (grepl("linux", sys_info["sysname"], ignore.case = TRUE)) {
+      assign("memory", TRUE, envir = has_logger)
+    }
+    logger::log_appender(logger::appender_file(paste0("smirfeTools_run_", substring(make.names(Sys.time()), 2), ".log")), namespace = "smirfeTools")
   }
   debugme::debugme()
 }
